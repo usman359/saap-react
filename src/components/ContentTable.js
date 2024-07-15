f13d1d039b832e3e96806378ad7446a224e86796
usman359 <m.usman095@gmail.com>
2024-07-15 17:36:53 +0500

    Commited

--- src/components/ContentTable.js
+++ src/components/ContentTable.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useTable } from "../contexts/TableContext";
 
 export default function ContentTable() {
+  // TableContext
+  const { checkboxItems } = useTable();
+
+  useEffect(() => {
+    console.log(checkboxItems);
+  }, [checkboxItems]);
+
   return (
     <main className="overflow-x-scroll">
       {/* Header container */}
@@ -9,7 +17,7 @@ export default function ContentTable() {
         <div className="flex gap-4 border-b outline-none">
           <label className="w-[10rem]">Items/Service Type</label>
           <select className="w-1/2 border border-gray-200">
-            <option>Item</option>
+            <option>Item1</option>
             <option>Item2</option>
           </select>
         </div>
@@ -26,60 +34,23 @@ export default function ContentTable() {
       <table className="w-full border-collapse">
         <thead>
           <tr className="bg-gray-200">
-            <th className="border border-gray-300 p-0.5">#</th>
-            <th className="border border-gray-300 p-0.5">Item No.</th>
-            <th className="border border-gray-300 p-0.5">Item Description</th>
-            <th className="border border-gray-300 p-0.5">Quantity</th>
-            <th className="border border-gray-300 p-0.5">Unit Price</th>
-            <th className="border border-gray-300 p-0.5">MRP</th>
-            <th className="border border-gray-300 p-0.5">Retail Price</th>
-            <th className="border border-gray-300 p-0.5">FED Rate</th>
-            <th className="border border-gray-300 p-0.5">Total (LC)</th>
-            <th className="border border-gray-300 p-0.5">Freight 2</th>
-            <th className="border border-gray-300 p-0.5">Freight 1 (LC)</th>
-            <th className="border border-gray-300 p-0.5">Gross Total</th>
-            <th className="border border-gray-300 p-0.5">Quality in Cartons</th>
-            <th className="border border-gray-300 p-0.5">CSDCode</th>
-            <th className="border border-gray-300 p-0.5">USCCode</th>
-            <th className="border border-gray-300 p-0.5">
-              Discount Percentage
-            </th>
-            <th className="border border-gray-300 p-0.5">FOC Amount</th>
-            <th className="border border-gray-300 p-0.5">Scheme Amount</th>
-            <th className="border border-gray-300 p-0.5">Tax Type</th>
-            <th className="border border-gray-300 p-0.5">MTB Amount</th>
-            <th className="border border-gray-300 p-0.5">Non 3rd</th>
-            <th className="border border-gray-300 p-0.5">Discount %</th>
-            <th className="border border-gray-300 p-0.5">UoM Code</th>
-            <th className="border border-gray-300 p-0.5">Whse</th>
+            {checkboxItems.map((item) => (
+              <th key={item} className="border border-gray-300 p-0.5">
+                {item.label}
+              </th>
+            ))}
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td className="border border-gray-300 bg-gray-200 p-0.5">&nbsp;</td>
-            <td className="border border-gray-300 p-0.5">&nbsp;</td>
-            <td className="border border-gray-300 p-0.5">&nbsp;</td>
-            <td className="border border-gray-300 p-0.5">&nbsp;</td>
-            <td className="border border-gray-300 p-0.5">&nbsp;</td>
-            <td className="border border-gray-300 p-0.5">&nbsp;</td>
-            <td className="border border-gray-300 p-0.5">&nbsp;</td>
-            <td className="border border-gray-300 p-0.5">&nbsp;</td>
-            <td className="border border-gray-300 p-0.5">&nbsp;</td>
-            <td className="border border-gray-300 p-0.5">&nbsp;</td>
-            <td className="border border-gray-300 p-0.5">&nbsp;</td>
-            <td className="border border-gray-300 p-0.5">&nbsp;</td>
-            <td className="border border-gray-300 p-0.5">&nbsp;</td>
-            <td className="border border-gray-300 p-0.5">&nbsp;</td>
-            <td className="border border-gray-300 p-0.5">&nbsp;</td>
-            <td className="border border-gray-300 p-0.5">&nbsp;</td>
-            <td className="border border-gray-300 p-0.5">&nbsp;</td>
-            <td className="border border-gray-300 p-0.5">&nbsp;</td>
-            <td className="border border-gray-300 p-0.5">&nbsp;</td>
-            <td className="border border-gray-300 p-0.5">&nbsp;</td>
-            <td className="border border-gray-300 p-0.5">&nbsp;</td>
-            <td className="border border-gray-300 p-0.5">&nbsp;</td>
-            <td className="border border-gray-300 p-0.5">&nbsp;</td>
-            <td className="border border-gray-300 p-0.5">&nbsp;</td>
+            {checkboxItems.map((item) => (
+              <td
+                key={item}
+                className="border border-gray-300 bg-gray-200 p-0.5"
+              >
+                &nbsp;
+              </td>
+            ))}
           </tr>
           <tr>
             <td className="border border-gray-300 bg-gray-200">&nbsp;</td>
